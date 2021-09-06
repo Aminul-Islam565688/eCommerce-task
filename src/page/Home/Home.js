50db7148a4d54bdf8ae3b78e5170a98abae9f855
Aminul islam <aminulislam565688@gmail.com>
2021-09-06 12:03:56 +0600

    top phone number and language done

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,10 +1,33 @@
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Home.scss';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className='home-main'>
+            <div className='site-setting'>
+                <div className='site-setting-wraper'>
+                    <div className='phone-number'>
+                        <a href="callto:+01900220033">
+                            <FontAwesomeIcon style={{ marginRight: '5px' }} icon={faPhoneAlt} />
+                            Call us: +01900220033
+                        </a>
+                    </div>
+                    <div className='setting-dropdown'>
+                        <select className='curency' name="" id="">
+                            <option value="">USD</option>
+                            <option value="">BDT</option>
+                            <option value="">EUR</option>
+                        </select>
+                        <select className='language' name="" id="">
+                            <option value="">Bangla</option>
+                            <option value="">English</option>
+                            <option value="">Roman</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
